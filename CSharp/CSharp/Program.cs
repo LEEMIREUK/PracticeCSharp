acf3b3aa015785675f41027b45f9088ded656098
Lee Mi Reuk <sslejds@naver.com>
2021-10-12 14:11:58 +0900

    for문과 break, continue의 쓰임
    for, break, continue의 사용방법

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -6,16 +6,35 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // 거울아 거울아~
-            string answer;
-
-            do
+            
+            // break -------------------------------
+            
+            int num = 10000002;
+            bool isPrime = true;
+            for(int i = 2; i< num; ++i)
             {
-                Console.WriteLine("나는 잘생겼나요? (y / n) : ");
-                answer = Console.ReadLine();
-            } while (answer != "y");
+                if(num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
 
-            Console.WriteLine("정답입니다.");
+            if(isPrime)
+                Console.WriteLine("소수입니다.");
+            else
+                Console.WriteLine("소수가 아닙니다.");
+        
+            //continue -----------------------------
+
+            for(int i = 1; i<= 100; ++i)
+            {
+                // continue를 활용해서 코드를 작성할 때 여러 조건들을 처리해주는것이 좋다.
+                if (i % 3 != 0)
+                    continue;
+
+                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
+            }
         }
     }
 }
