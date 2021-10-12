073dd25e4e12242f12646dc5c3d41f9c51d6f280
Lee Mi Reuk <sslejds@naver.com>
2021-10-12 14:38:40 +0900

    함수 사용
    함수 사용방법

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,37 +4,36 @@ namespace CSharp
 {
     class Program
     {
+        //static 반환형식 이름(매개변수목록)
+        //{
+        //}
+        static void HellowWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        // 덧셈 함수
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static void AddOne(ref int number)
+        {
+            // 주소값을 넘겨 값 변경이 가능하다.
+            number += 1;
+        }
+
         static void Main(string[] args)
         {
-            
-            // break -------------------------------
-            
-            int num = 10000002;
-            bool isPrime = true;
-            for(int i = 2; i< num; ++i)
-            {
-                if(num % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if(isPrime)
-                Console.WriteLine("소수입니다.");
-            else
-                Console.WriteLine("소수가 아닙니다.");
-        
-            //continue -----------------------------
-
-            for(int i = 1; i<= 100; ++i)
-            {
-                // continue를 활용해서 코드를 작성할 때 여러 조건들을 처리해주는것이 좋다.
-                if (i % 3 != 0)
-                    continue;
-
-                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
-            }
+            int a = 4;
+            int b = 5;
+            int num = 0;
+            HellowWorld();
+            int result = Add(a, b);
+            // 참조(ref)   복사(ref없는것)
+            Program.AddOne(ref num);
         }
     }
 }
