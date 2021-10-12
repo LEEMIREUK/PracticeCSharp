94fe99573d3c047c72219a1f5799812ec3bf1cca
Lee Mi Reuk <sslejds@naver.com>
2021-10-12 13:02:51 +0900

    가위바위보 게임
    지금까지 배운 걸로 가위바위보 게임을 만들어보자.

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -7,17 +7,110 @@ namespace CSharp
         // 주석을 달 수 있다.
         static void Main(string[] args)
         {
-            // byte(1바이트 0 ~ 255), short(2바이트 -3만 ~ 3만), int(4바이트 -21억 ~ 21억), long(8바이트)
-            // sbyte(1바이트 -128 ~ 127), ushort(2바이트 0 ~ 6만), uint(4바이트 0 ~ 43억), ulong(8바이트)
+            //// 삼항 연산자
+            //int number = 25;
+            ////(조건 ? 참인경우 : 거짓인경우)
+            //bool isPair = ((number % 2) == 0 ? true : false);
 
+            // 0: 가위 1: 바위 2: 보
 
-            // 1. 바구니의 크기가 다른 경우
-            int a = 0x0FFFFFFF;
-            short b = (short)a;
+            Random rand = new Random();
+            int aiChoice = rand.Next(0, 3); // 0 ~ 2 사이의 랜덤 값
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-            // ﻿2. 바구니의 크기는 같지만 부호가 다른 경우
-            byte c = 255;
-            sbyte sb = (sbyte)c;
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("당신은 가위입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("당신은 바위입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("당신은 보입니다.");
+                    break;
+            }
+
+            switch (aiChoice)
+            {
+                case 0:
+                    Console.WriteLine("컴퓨터의 가위입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("컴퓨터의 바위입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("컴퓨터의 보입니다.");
+                    break;
+            }
+
+            // 승리 무승부 패배
+            //if (choice == 0)
+            //{
+            //    if (aiChoice == 0)
+            //    {
+            //        Console.WriteLine("무승부입니다.");
+            //    }
+            //    else if(aiChoice == 1)
+            //    {
+            //        Console.WriteLine("패배입니다..");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("승리입니다..");
+            //    }
+            //}
+            //else if (choice == 1)
+            //{
+            //    if (aiChoice == 0)
+            //    {
+            //        Console.WriteLine("승리입니다.");
+            //    }
+            //    else if (aiChoice == 1)
+            //    {
+            //        Console.WriteLine("무승부입니다.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("패배입니다.");
+            //    }
+            //}
+            //else
+            //{
+            //    if (aiChoice == 0)
+            //    {
+            //        Console.WriteLine("패배입니다.");
+            //    }
+            //    else if (aiChoice == 1)
+            //    {
+            //        Console.WriteLine("승리입니다.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("무승부입니다.");
+            //    }
+            //}
+
+            if(choice == aiChoice)
+            {
+                Console.WriteLine("무승부입니다.");
+            }
+            else if (choice == 0 &&  aiChoice == 2)
+            {
+                Console.WriteLine("승리입니다.");
+            }
+            else if (choice == 1 && aiChoice == 0)
+            {
+                Console.WriteLine("승리입니다.");
+            }
+            else if (choice == 2 && aiChoice == 1)
+            {
+                Console.WriteLine("승리입니다.");
+            }
+            else
+            {
+                Console.WriteLine("패배입니다.");
+            }
         }
     }
 }
