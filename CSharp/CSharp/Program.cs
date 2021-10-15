1aaf5352ea00a722741a40938a6a9535224ac5d8
Lee Mi Reuk <sslejds@naver.com>
2021-10-15 12:59:39 +0900

    class type convert
    클래스의 타입 변환

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -5,65 +5,42 @@ namespace CSharp
     // 객체 (OOP Object Oriented Programming)
     // 객체지향의 3대 속성 (은닉성/상속성/다형성)
 
-    class Player    // 부모 / 기반 클래스
+    class Player
     {
-        static public int counter = 1;    // 오로지 1개만 존재
+        protected int hp;
+        protected int attack;
+    }
+    class Knight : Player
+    {
 
-        public int id;
-        public int hp;
-        public int attack;
-
-        public Player()
-        {
-            Console.WriteLine("Player 생성자 호출");
-        }
-
-        public Player(int hp)
-        {
-            this.hp = hp;
-            Console.WriteLine("Player hp 생성자 호출");
-        }
-        
-        public void Move()
-        {
-            Console.WriteLine("Knight Move");
-        }
-        public void Attack()
-        {
-            Console.WriteLine("Knight Attack");
-        }
     }
     class Mage : Player
     {
-
-    }
-
-    class Archer : Player
-    {
-
-    }
-
-    class Knight : Player   // 자식 / 파생 클래스
-    {
-        public Knight() : base(100)
-        {
-            base.hp = 100;
-            Console.WriteLine("Knight 생성자 호출");
-        }
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 100;
-            return knight;
-        }
+        public int mp;
     }
 
     class Program
     {
+        static void EnterGame(Player player)
+        {
+            Mage mage = (player as Mage);
+            if(mage != null)
+            {
+                mage.mp = 10;
+            }
+        }
+
         static void Main(string[] args)
         {
             Knight knight = new Knight();
+            Mage mage = new Mage();
+
+            // Mage 타입 -> Player 타입 변환가능
+            // Player 타입 -> Mage 타입 변환 바로 불가능, 명시를 해줘야함
+            Player magePlayer = mage;
+            Mage mage2 = (Mage)magePlayer;
+
+            EnterGame(knight);
         }
     }
 }
