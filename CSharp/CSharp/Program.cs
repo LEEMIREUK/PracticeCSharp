70febb767139f6a040f7daade3db90e6eaf7005e
Lee Mi Reuk <sslejds@naver.com>
2021-10-16 05:29:02 +0900

    Practice
    연습 문제

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,27 +4,83 @@ namespace CSharp
 {
     class Program
     {
+        static int GetHightestScore(int[] scores)
+        {
+            int max = 0;
+            foreach(int score in scores)
+            {
+                if (max < score)
+                    max = score;
+            }
+            return max;
+        }
+
+        static int GetAverageScore(int[] scores)
+        {
+            if (scores.Length == 0)
+                return 0;
+
+            int sum = 0;
+            foreach (int score in scores)
+                sum += score;
+
+            return sum / scores.Length;
+        }
+
+        static int GetIndexOf(int[] scores, int value)
+        {
+            for(int i = 0; i< scores.Length; ++i)
+            {
+                if (scores[i] == value)
+                    return i;
+            }
+            return -1;
+        }
+
+        static void Sort(int[] scores)
+        {
+            for( int i = 0; i< scores.Length; ++i)
+            {
+                // 제일 작은 숫자가 있는 index찾기
+                int minIndex = i;
+                for (int j = i; j < scores.Length; ++j)
+                {
+                    if (scores[j] < scores[minIndex])
+                        minIndex = j;
+                }
+
+                int temp = scores[i];
+                scores[i] = scores[minIndex];
+                scores[minIndex] = temp;
+            }
+            //for (int i = 0; i < scores.Length; ++i)
+            //{
+            //    for (int j = 0; j < scores.Length - 1; ++j)
+            //    {
+            //        if (scores[j] > scores[j + 1])
+            //        {
+            //            int temp = scores[j + 1];
+            //            scores[j] = scores[j + 1];
+            //            scores[j] = temp;
+            //        }
+            //    }
+            //}
+        }
+
         static void Main(string[] args)
         {
             // 배열
-            int[] scores = new int[] { 10, 20, 30, 40, 50 };
+            int[] scores = new int[] { 10, 30, 40, 20, 50 };
+            int highestScore = GetHightestScore(scores);
+            Console.WriteLine(highestScore);
 
-            // 0 1 2 3 4
-            //scores[0] = 10;
-            //scores[1] = 20;
-            //scores[2] = 30;
-            //scores[3] = 40;
-            //scores[4] = 50;
+            int averageScore = GetAverageScore(scores);
+            Console.WriteLine(averageScore);
 
-            for (int i = 0; i < scores.Length; ++i)
-            {
-                Console.WriteLine(scores[i]);
-            }
+            int index = GetIndexOf(scores, 20);
+            Console.WriteLine(index);
 
-            foreach(int score in scores)
-            {
-                Console.WriteLine(score);
-            }
+            Sort(scores);
         }
     }
 }
