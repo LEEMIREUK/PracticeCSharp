5a3e3bac14c6006a0606998220a66fbef73050b2
Lee Mi Reuk <sslejds@naver.com>
2021-10-18 04:43:28 +0900

    Delegate
    델리게이트 공부

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -5,17 +5,37 @@ namespace CSharp
 {
     class Program
     {
-        // 객체지향 -> 은닉성
-        class Knight
+        delegate int OnClicked();   // 이것은 함수가 아니라 형식이다.
+        // delegate -> 형식은 형식인데, 함수 자체를 인자로 넘겨주는 형식
+        // 반환: int 입력: void
+        // OnClicked 이 delegate 형식의 이름이다.
+
+        static void ButtonPressed(OnClicked clickedFunction/* 함수 자체를 인자로 넘겨주고 */)
         {
-            public int Hp { get; set; } = 100;
+            // 함수를 호출();
+            clickedFunction();
         }
-        
+
+        static int TestDelegate()
+        {
+            Console.WriteLine("Hello Delegate");
+            return 0;
+        }
+        static int TestDelegate2()
+        {
+            Console.WriteLine("Hello Delegate2");
+            return 0;
+        }
+
         static void Main(string[] args)
         {
-            Knight knight = new Knight();
+            // delegate (대리자)
+            Console.WriteLine();
 
-            int hp = knight.Hp;
+            OnClicked clicked = new OnClicked(TestDelegate);
+            clicked += TestDelegate2;
+
+            //ButtonPressed(TestDelegate);
         }
     }
 }
