6d3606bff12196595b6d05600eec148336c1358d
Lee Mi Reuk <sslejds@naver.com>
2021-10-18 15:02:46 +0900

    event
    event를 이용한 delegate

---
+++ CSharp/CSharp/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    // Observer Pattern
+    class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if (Console.KeyAvailable)
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                // 모두에게 알려준다.
+                InputKey();
+            }
+        }
+    }
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -5,37 +5,20 @@ namespace CSharp
 {
     class Program
     {
-        delegate int OnClicked();   // 이것은 함수가 아니라 형식이다.
-        // delegate -> 형식은 형식인데, 함수 자체를 인자로 넘겨주는 형식
-        // 반환: int 입력: void
-        // OnClicked 이 delegate 형식의 이름이다.
-
-        static void ButtonPressed(OnClicked clickedFunction/* 함수 자체를 인자로 넘겨주고 */)
+        static void OnInputTest()
         {
-            // 함수를 호출();
-            clickedFunction();
+            Console.WriteLine("Input Received");
         }
-
-        static int TestDelegate()
-        {
-            Console.WriteLine("Hello Delegate");
-            return 0;
-        }
-        static int TestDelegate2()
-        {
-            Console.WriteLine("Hello Delegate2");
-            return 0;
-        }
-
         static void Main(string[] args)
         {
-            // delegate (대리자)
-            Console.WriteLine();
+            InputManager inputManager = new InputManager();
 
-            OnClicked clicked = new OnClicked(TestDelegate);
-            clicked += TestDelegate2;
+            inputManager.InputKey += OnInputTest;
 
-            //ButtonPressed(TestDelegate);
+            while(true)
+            {
+                inputManager.Update();
+            }
         }
     }
 }
