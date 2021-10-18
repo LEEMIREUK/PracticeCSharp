07b507b17eedb8410c3c526607ebc8d4b50296ba
Lee Mi Reuk <sslejds@naver.com>
2021-10-18 20:22:43 +0900

    Lambda
    람다식 공부

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -3,22 +3,100 @@ using System.Collections.Generic;
 
 namespace CSharp
 {
+    enum ItemType
+    {
+        Weapon,
+        Armor,
+        Amulet,
+        Ring
+    }
+
+    enum Rarity
+    {
+        Normal,
+        Uncommon,
+        Rare
+    }
+
+    class Item
+    {
+        public ItemType ItemType;
+        public Rarity Rarity;
+    }
+
+
     class Program
     {
-        static void OnInputTest()
+        static List<Item> _items = new List<Item>();
+
+        delegate bool ItemSelector(Item item);
+        //delegate Return MyFunc<T, Return>(T item);
+
+        static bool IsWeapon(Item item)
         {
-            Console.WriteLine("Input Received");
+            return item.ItemType == ItemType.Weapon;
         }
+
+        static Item FindItem(Func<Item, bool> selector)
+        {
+            foreach (Item item in _items)
+            {
+                if (selector(item))
+                    return item;
+            }
+            return null;
+        }
+
+
+        static Item FindItem(ItemSelector selector)
+        {
+            foreach (Item item in _items)
+            {
+                if (selector(item))
+                    return item;
+            }
+            return null;
+        }
+
+        //static Item FindWeapon()
+        //{
+        //    foreach (Item item in _items)
+        //    {
+        //        if (item.ItemType == ItemType.Weapon)
+        //            return item;
+        //    }
+        //    return null;
+        //}
+
+        //static Item FindRareItem()
+        //{
+        //    foreach (Item item in _items)
+        //    {
+        //        if (item.Rarity == Rarity.Rare)
+        //            return item;
+        //    }
+        //    return null;
+        //}
+
         static void Main(string[] args)
         {
-            InputManager inputManager = new InputManager();
+            _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
+            _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
+            _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
 
-            inputManager.InputKey += OnInputTest;
+            // Lambda : 일회용 함수를 만드는데 사용하는 문법이다.
 
-            while(true)
-            {
-                inputManager.Update();
-            }
+            //Item item = FindItem(IsWeapon);
+
+            ItemSelector selector = new ItemSelector((Item item) => { return item.ItemType == ItemType.Weapon; });
+
+            // Anonymous Function: 무명함수 / 익명함수
+            Item item = FindItem(delegate (Item item) { return item.ItemType == ItemType.Weapon; });
+
+            // Lambda
+            Item item2 = FindItem((Item item) => { return item.ItemType == ItemType.Weapon; });
+
+            Func<Item, bool> selector2 = (Item item) => { return item.ItemType == ItemType.Weapon; };
         }
     }
 }
