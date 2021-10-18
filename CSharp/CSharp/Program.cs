2d0f35801b4604581440450b25114b059d16947b
Lee Mi Reuk <sslejds@naver.com>
2021-10-18 22:55:48 +0900

    Exception
    예외처리 공부

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -3,100 +3,41 @@ using System.Collections.Generic;
 
 namespace CSharp
 {
-    enum ItemType
-    {
-        Weapon,
-        Armor,
-        Amulet,
-        Ring
-    }
-
-    enum Rarity
-    {
-        Normal,
-        Uncommon,
-        Rare
-    }
-
-    class Item
-    {
-        public ItemType ItemType;
-        public Rarity Rarity;
-    }
-
-
     class Program
     {
-        static List<Item> _items = new List<Item>();
-
-        delegate bool ItemSelector(Item item);
-        //delegate Return MyFunc<T, Return>(T item);
-
-        static bool IsWeapon(Item item)
+        class TestException : Exception
         {
-            return item.ItemType == ItemType.Weapon;
+
         }
-
-        static Item FindItem(Func<Item, bool> selector)
-        {
-            foreach (Item item in _items)
-            {
-                if (selector(item))
-                    return item;
-            }
-            return null;
-        }
-
-
-        static Item FindItem(ItemSelector selector)
-        {
-            foreach (Item item in _items)
-            {
-                if (selector(item))
-                    return item;
-            }
-            return null;
-        }
-
-        //static Item FindWeapon()
-        //{
-        //    foreach (Item item in _items)
-        //    {
-        //        if (item.ItemType == ItemType.Weapon)
-        //            return item;
-        //    }
-        //    return null;
-        //}
-
-        //static Item FindRareItem()
-        //{
-        //    foreach (Item item in _items)
-        //    {
-        //        if (item.Rarity == Rarity.Rare)
-        //            return item;
-        //    }
-        //    return null;
-        //}
 
         static void Main(string[] args)
         {
-            _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
-            _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
-            _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
+            try
+            {
+                // 1. 0으로 나눌 때
+                // 2. 잘못된 메모리를 참조(null)
+                // 3. 오버플로우
 
-            // Lambda : 일회용 함수를 만드는데 사용하는 문법이다.
+                //int a = 10;
+                //int b = 0;
+                //int result = a / b;
 
-            //Item item = FindItem(IsWeapon);
+                //int c = 0;
 
-            ItemSelector selector = new ItemSelector((Item item) => { return item.ItemType == ItemType.Weapon; });
+                throw new TestException();
+            }
+            catch (DivideByZeroException e)
+            {
 
-            // Anonymous Function: 무명함수 / 익명함수
-            Item item = FindItem(delegate (Item item) { return item.ItemType == ItemType.Weapon; });
+            }
+            catch (Exception e)
+            {
 
-            // Lambda
-            Item item2 = FindItem((Item item) => { return item.ItemType == ItemType.Weapon; });
-
-            Func<Item, bool> selector2 = (Item item) => { return item.ItemType == ItemType.Weapon; };
+            }
+            finally
+            {
+                // DB, 파일 정리 등등
+            }
         }
     }
 }
